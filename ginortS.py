7931131f672b3f7b75e9fac7bfc91beec3f85bb4
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-03 09:44:22 -0500

    Update ginortS.py

--- ginortS.py
+++ ginortS.py
@@ -1,2 +1,7 @@
+# Python 3 code
+# Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+
 myList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1357902468"
 print(*sorted(input(), key = myList.index), sep ="")

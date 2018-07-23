674aa372c029b71b67120e23e7ac54b84ea3fe71
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-23 09:57:42 -0500

    Update StandardDeviation.py

--- StandardDeviation.py
+++ StandardDeviation.py
@@ -1,3 +1,8 @@
+# Python 3 code
+# Bruce Adams
+# austingamestudios.com
+# ezaroth@gmail.com
+
 n = int(input())
 nums = [int(x) for x in input().strip().split()]
 print(round((sum([(x-(sum(nums) / n))**2 for x in nums])/n)**.5, 1))

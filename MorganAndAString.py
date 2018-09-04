c3bb3d9fe2eb588623d33642055b37f3e33fc6f4
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-04 09:25:34 -0500

    Add files via upload

---
+++ MorganAndAString.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the morganAndString function below.
+def morganAndString(a, b):
+    answer = ''
+    a += '~'
+    b += '~'
+    i = 0
+    j = 0
+    while a[i] != '~' or b[j] != '~':
+        if a[i] != '~' and a[i:] < b[j:]:
+            answer += a[i]
+            i += 1
+        else:
+            answer += b[j]
+            j += 1
+    return answer  
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        a = input()
+
+        b = input()
+
+        result = morganAndString(a, b)
+
+        fptr.write(result + '\n')
+
+    fptr.close()

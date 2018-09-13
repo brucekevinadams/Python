55f2fe4273eafdd7e2fe9e18d206e5778069edba
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-13 10:41:32 -0500

    Add files via upload

---
+++ MinimumLoss.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+n = int(input().strip())
+numbers = list(map(int, input().strip().split()))
+myDict = {}
+for i in range(n):
+  myDict[numbers[i]] = i
+minCost = 10 ** 10
+nums = sorted(myDict)
+for i in range(1, n):
+  if (nums[i] - nums[i - 1] < minCost) and(myDict[nums[i]] < myDict[nums[i - 1]]):
+    minCost = nums[i] - nums[i - 1]
+print(minCost)

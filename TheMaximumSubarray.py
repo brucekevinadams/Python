5c7e3e719941af600abc47d10ef587c2c69093f4
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-26 07:36:33 -0500

    Add files via upload

---
+++ TheMaximumSubarray.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+for _ in range(int(input())):
+    _, arr = input(), [int(n) for n in input().split()]
+    h = m = t = arr[0]
+    for ind, n in enumerate(arr):
+        if ind == 0: continue
+        t = max(t, n, t + n)
+        h = max(n, h + n)
+        m = max(m, h)
+    print(m, t)

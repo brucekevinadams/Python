a53a43e3b3f05dc7aa2a0b57f352721eacfdc417
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-04 08:38:29 -0500

    Add files via upload

---
+++ CountingSort1.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+__ = input()
+
+sol = [0] * 100
+for i in [int(n) for n in input().split()]:
+    sol[i] += 1
+
+print(*sol)

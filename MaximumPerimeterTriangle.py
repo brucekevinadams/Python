4628321da306ab00595d0170f569b0842f8aacbb
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-16 07:27:57 -0500

    Update MaximumPerimeterTriangle.py

--- MaximumPerimeterTriangle.py
+++ MaximumPerimeterTriangle.py
@@ -3,6 +3,18 @@
 # website: austingamestudios.com
 # Python 3 program
 # Hackerrank problem
+# Given an array of stick lengths, use 3 of them to construct a non-degenerate triange
+# with the maximum possible perimeter. Print the lengths of its sides as 3 space-separated
+# integers in non-decreasing order.
+#
+# If there are several valid triangles having the maximum perimeter:
+#
+# Choose the one with the longest maximum side.
+# If more than one has that maximum, choose from them the one with the longest minimum side.
+# If more than one has that maximum as well, print any one them.
+#
+# If no non-degenerate triangle exists, print -1.
+#
 #!/bin/python3
 
 import math
